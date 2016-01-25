7f6cc760c7f0f3b7e3051eb4668895b015a55b41
Ioannis Lazaridis <ounos@live.com>
2016-01-25 16:58:45 +0100

    Update openresty + minor fixes

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-VERSION=${1:-"1.9.3.2"}
+VERSION=${1:-"1.9.7.2"}
 PKG=ngx_openresty-$VERSION
 
 apt-get update
-apt-get install -y libpcre3-dev build-essential libssl-dev sudo libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make
+apt-get install -y libpcre3-dev build-essential libssl-dev sudo libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make git > /dev/null 2>&1
 
 #Install openresty
 wget http://openresty.org/download/${PKG}.tar.gz
@@ -16,7 +16,7 @@ make install
 
 # Install packages
 apt-get install -y lua5.1 luarocks
-luarocks install moonscript
+luarocks install lpeg alt-getopt luafilesystem moonscript
 luarocks install lapis
 
 #Create new lapis project
